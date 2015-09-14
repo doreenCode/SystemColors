202f87ba323fd7c697009fef2ec53763af9de44f
DCT <doreen@steerlist.com>
2015-09-14 09:39:13 -0700

    Initial Commit

---
+++ SystemColors/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SystemColors
+//
+//  Created by Doreen Theverapperuma on 9/14/15.
+//  Copyright © 2015 DCT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SystemColors/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SystemColors
+//
+//  Created by Doreen Theverapperuma on 9/14/15.
+//  Copyright © 2015 DCT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
